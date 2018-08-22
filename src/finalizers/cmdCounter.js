f830fa0419f164950390bb6c2b2a8ca8bd178f9a
1Conan <me@1conan.com>
2018-08-22 10:30:17 +0000

    yet another dumb mistake

--- src/finalizers/cmdCounter.js
+++ src/finalizers/cmdCounter.js
@@ -23,6 +23,6 @@ module.exports = class extends Finalizer {
       executions,
     });
     this.client.stats.commands.overall.count += 1;
-    this.client.stats.commandslastMinute += 1;
+    this.client.stats.commands.lastMinute += 1;
   }
 };
