049b6f8178d064f27800fb5e01489302ab1119b1
1Conan <me@1conan.com>
2018-08-23 06:56:26 +0000

    change type of overall count

--- src/lib/structures/defaultClientSchema.js
+++ src/lib/structures/defaultClientSchema.js
@@ -3,12 +3,12 @@ const { Client } = require('klasa');
 Client.defaultClientSchema
   .add('commands', (folder) => {
     folder
-      .add('overall', 'integer', { default: 0 })
+      .add('overall', 'number', { default: 0 })
       .add('ran', 'any', { default: {} })
       .add('lastMinute', 'any', { array: true, default: [] });
   })
   .add('messages', (folder) => {
     folder
-      .add('overall', 'integer', { default: 0 })
+      .add('overall', 'number', { default: 0 })
       .add('lastMinute', 'any', { array: true, default: [] });
   });
